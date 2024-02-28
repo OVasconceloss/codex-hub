84b34d6ecfbe540125d51757a0f30ed42cbe6007
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-28 08:49:49 +0000

    :sparkles: feat: create the file for the API routes

---
+++ server/src/routes.ts
@@ -0,0 +1,10 @@
+import { 
+    FastifyInstance, 
+    FastifyPluginOptions, 
+    FastifyRequest, 
+    FastifyReply 
+} from "fastify";
+
+export async function fastifyRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    
+}
