b244064b6e549e4b405a25a8747af63ce7177ec8
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-28 08:50:22 +0000

    :sparkles: feat: import the route file and set the cors configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import { fastifyRoutes } from './routes';
 
 const fastifyServer = Fastify({ logger: true });
 
 const startServer = async () => {
+    await fastifyServer.register(fastifyCors);
+    await fastifyServer.register(fastifyRoutes);
+
     try {
         await fastifyServer.listen({ port: 8080 });
         console.log(`The server was started on PORT 8080`);
