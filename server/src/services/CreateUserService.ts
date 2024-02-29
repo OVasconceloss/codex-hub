62befca95234c7241d4a972be94c13821f5f934b
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-29 22:44:45 +0000

    :sparkles: feat: create a service to add the data to the database

--- server/src/services/CreateUserService.ts
+++ server/src/services/CreateUserService.ts
@@ -1,8 +1,25 @@
 import prismaClient from "../prisma/connectPrisma";
 
+interface UserProps {
+    name: string,
+    email: string
+};
+
 class CreateUserService {
-    async execute() {
-        console.log("TEST");
+    async execute({ name, email }: UserProps) {
+        if (!name || !email) {
+            throw new Error("The name and email field need to be defined");
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                name,
+                email,
+                status: true
+            }
+        });
+
+        return user;
     };
 };
 
