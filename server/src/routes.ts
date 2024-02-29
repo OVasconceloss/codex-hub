3f01801d7cf46572c6cfa28a9043fe0f38430fee
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-02-29 22:46:25 +0000

    :sparkles: feat: create the route to create the user

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,7 +4,12 @@ import {
     FastifyRequest, 
     FastifyReply 
 } from "fastify";
+import { CreateUserController } from "./controllers/CreateUserController";
 
 export async function fastifyRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-    
-}
+
+    //ROUTE FOR CREATE USER
+    fastify.post('/user', async (request: FastifyRequest, response: FastifyReply) => {
+        return new CreateUserController().handleCreateUser(request, response);
+    });
+};
