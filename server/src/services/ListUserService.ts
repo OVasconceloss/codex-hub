cf19fb5ffae33966e9e2fb0fa7ca0f4707212734
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-01 21:50:06 +0000

    :sparkles: feat: create the service to list all users

---
+++ server/src/services/ListUserService.ts
@@ -0,0 +1,10 @@
+import prismaClient from "../prisma/connectPrisma";
+
+class ListUserService {
+    async excute() {
+        const users = await prismaClient.user.findMany();
+        return users;
+    };
+};
+
+export { ListUserService };
