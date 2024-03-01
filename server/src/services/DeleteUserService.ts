54c4572478c4309e034fe66be73865c969125fa0
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-01 22:15:09 +0000

    :sparkles: feat: create the service to delete a user

---
+++ server/src/services/DeleteUserService.ts
@@ -0,0 +1,19 @@
+import prismaClient from "../prisma/connectPrisma";
+
+interface UserProps {
+    userId: string
+};
+
+class DeleteUserService {
+    async execute({ userId }: UserProps) {
+        const deleteUser = await prismaClient.user.delete({
+            where: {
+                id: userId
+            }
+        });
+
+        return deleteUser;
+    };
+};
+
+export { DeleteUserService };
