e2cab092b409f20474a467dec9dcf9959331c034
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-01 22:15:41 +0000

    :sparkles: feat: create the controller to delete a user

---
+++ server/src/controllers/DeleteUserController.ts
@@ -0,0 +1,19 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { DeleteUserService } from '../services/DeleteUserService';
+
+interface UserProps {
+    userId: string
+};
+
+class DeleteUserController {
+    async handleDeleteUser(request: FastifyRequest, response: FastifyReply) {
+        const { userId } = request.body as UserProps;
+
+        const deleteService = new DeleteUserService();
+        const deleteUser = await deleteService.execute({ userId });
+
+        response.send({message: 'User deleted successfully!'});
+    };
+};
+
+export { DeleteUserController };
