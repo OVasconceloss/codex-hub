f7c6c3fa7958bfe913fdfd83a1317608b4aaa5b1
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-01 21:51:09 +0000

    :sparkles: feat: create the route to list the users

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import {
     FastifyRequest, 
     FastifyReply 
 } from "fastify";
+import { ListUserController } from "./controllers/ListUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 export async function fastifyRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
@@ -12,4 +13,13 @@ export async function fastifyRoutes(fastify: FastifyInstance, options: FastifyPl
     fastify.post('/user', async (request: FastifyRequest, response: FastifyReply) => {
         return new CreateUserController().handleCreateUser(request, response);
     });
+
+    //ROUTE FOR SHOW USER
+    fastify.get('/users', async (request: FastifyRequest, response: FastifyReply) => {
+        return new ListUserController().handleListUsers(request, response);
+    });
+
+    //ROUTE FOR UPDATE USER
+
+    //ROUTE FOR DELETE USER
 };
