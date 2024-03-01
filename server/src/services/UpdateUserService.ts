533180c9d6629dc575548dd782761c3e6aa31ed2
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-01 22:56:29 +0000

    :sparkles: feat: create the service to update a user

---
+++ server/src/services/UpdateUserService.ts
@@ -0,0 +1,32 @@
+import prismaClient from "../prisma/connectPrisma";
+
+interface UserProps {
+    name: string,
+    email: string,
+    userId: string,
+};
+
+class UpdateUserService {
+    async execute({ userId, name, email }: UserProps) {
+        if (!name || !email)
+            throw new Error("The name and email field need to be defined");
+
+        const findUser = await prismaClient.user.findMany({ where: { id: userId } });
+
+        if (findUser) {
+            const updateUser = await prismaClient.user.update({
+                where: {
+                    id: userId,
+                },
+                data: {
+                    name: name,
+                    email: email
+                }
+            });
+
+            return updateUser;
+        };
+    };
+};
+
+export { UpdateUserService };
