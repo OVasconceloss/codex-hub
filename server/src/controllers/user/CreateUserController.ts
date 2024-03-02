d8de93e7ac2bac4cdf97af06bba49d0098bd24c8
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-02 20:47:43 +0000

    :sparkles: feat: hash the password coming from the client

--- server/src/controllers/user/CreateUserController.ts
+++ server/src/controllers/user/CreateUserController.ts
@@ -1,17 +1,22 @@
+import bcrypt from 'bcrypt';
 import { FastifyRequest, FastifyReply } from "fastify";
 import { CreateUserService } from "../../services/user/CreateUserService";
 
 interface UserProps {
     name: string,
-    email: string
+    email: string,
+    password: string,
+    hashedPassword: string,
 };
 
 class CreateUserController {
     async handleCreateUser(request: FastifyRequest, response: FastifyReply) {
-        const { name, email } = request.body as UserProps;
+        const { name, email, password } = request.body as UserProps;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         const userService = new CreateUserService();
-        const user = await userService.execute({ name, email });
+        const user = await userService.execute({ name, email, hashedPassword });
 
         response.send(user);
     };
