179677d83b75a0c0373c5138bf92d3463b4ce947
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-02 22:31:35 +0000

    :sparkles: feat: create the controller for the login service

---
+++ server/src/controllers/user/auth/LoginUserController.ts
@@ -0,0 +1,20 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { LoginUserSerivce } from "../../../services/user/auth/LoginUserService";
+
+interface UserProps {
+    name: string,
+    password: string
+}
+
+class LoginUserController {
+    async handleLoginUser(request: FastifyRequest, response: FastifyReply) {
+        const { name, password } = request.body as UserProps;
+
+        const loginService = new LoginUserSerivce();
+        const login = await loginService.execute({ name, password });
+
+        response.send(login);
+    };
+};
+
+export { LoginUserController };
