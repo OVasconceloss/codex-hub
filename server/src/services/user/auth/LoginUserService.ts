9e8dfe69d8f3311d9069bcc6abf3d997f26be96c
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-03 10:20:45 +0000

    :sparkles: feat: create the verification process for the login

--- server/src/services/user/auth/LoginUserService.ts
+++ server/src/services/user/auth/LoginUserService.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import bcrypt from 'bcrypt';
+import { FastifyReply } from 'fastify';
 import jasonWebToken from 'jsonwebtoken';
 import prismaClient from "../../../prisma/connectPrisma";
 
@@ -9,19 +10,28 @@ interface UserProps {
 };
 
 class LoginUserSerivce {
-    async execute({password, name}: UserProps) {
+    async execute({password, name}: UserProps, response: FastifyReply) {
         const finduser = await prismaClient.user.findFirst({ where: { name: name } });
 
         if (finduser) {
-            if (!process.env.TOKEN_SECRET) console.log('error');
-            else {
-                const matchPassword = await bcrypt.compare(password, finduser.password);
-                const accessToken = jasonWebToken.sign(JSON.stringify(finduser), process.env.TOKEN_SECRET);
+            if (!process.env.TOKEN_SECRET) {
+                console.log('Error: TOKEN_SECRET Not Found');
+                return response.code(500).send({ error: 'Internal Server Error'});
+            }
+            
+            const matchPassword = await bcrypt.compare(password, finduser.password);
     
-                if (matchPassword) return { accessToken: accessToken };
-                else return { message: "Invalid Credentials" };
+            if (matchPassword) {
+                const accessToken = jasonWebToken.sign(JSON.stringify(finduser), process.env.TOKEN_SECRET);
+                response.header('authorization', `Bearer ${accessToken}`);
+
+                return response.send({ accessToken });
+            } else {
+                return response.code(401).send({ error: "Invalid Credentials" });
             };
-        };
+        } else {
+            return response.code(404).send({ error: 'User Not Found' });
+        }
     };
 };
 
