dfaf4afafd27f7375cdc6c054f71eb7c1c65a065
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-03 10:25:39 +0000

    :sparkles: feat: add the user authentication middleware to protect the routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,38 +2,44 @@ import {
     FastifyInstance, 
     FastifyPluginOptions, 
     FastifyRequest, 
-    FastifyReply 
+    FastifyReply,
+     
 } from "fastify";
+import { authenticateUser } from "./middlewares/userAuth";
+import { FastifyRequestType } from "fastify/types/type-provider";
 import { ListUserController } from "./controllers/user/ListUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DeleteUserController } from "./controllers/user/DeleteUserController";
 import { UpdateUserController } from "./controllers/user/UpdateUserController";
 import { LoginUserController } from "./controllers/user/auth/LoginUserController";
 
+
+interface CustomRequest extends FastifyRequestType { accessToken: any; }
+declare module 'fastify' { interface FastifyRequest extends CustomRequest {} }
+
 export async function fastifyRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-
     //ROUTE FOR CREATE USER
-    fastify.post('/user', async (request: FastifyRequest, response: FastifyReply) => {
+    fastify.post('/register', async (request: FastifyRequest, response: FastifyReply) => {
         return new CreateUserController().handleCreateUser(request, response);
-    });
-
-    //ROUTE FOR SHOW USER
-    fastify.get('/users', async (request: FastifyRequest, response: FastifyReply) => {
-        return new ListUserController().handleListUsers(request, response);
-    });
-
-    //ROUTE FOR UPDATE USER
-    fastify.put('/user/update', async (request: FastifyRequest, response: FastifyReply) =>{
-        return new UpdateUserController().handleUpdateUser(request, response);
-    });
-
-    //ROUTE FOR DELETE USER
-    fastify.post('/user/delete', async (request: FastifyRequest, response: FastifyReply) => {
-        return new DeleteUserController().handleDeleteUser(request, response);
     });
 
     //ROUTE FOR LOGIN USER
     fastify.post('/user/login', async (request: FastifyRequest, response: FastifyReply) => {
         return new LoginUserController().handleLoginUser(request, response);
+    });
+
+    //ROUTE FOR SHOW USER
+    fastify.get('/users', { preHandler: authenticateUser }, async (request: FastifyRequest, response: FastifyReply) => {
+        return new ListUserController().handleListUsers(request, response);
+    });
+
+    //ROUTE FOR UPDATE USER
+    fastify.put('/user/update', { preHandler: authenticateUser }, async (request: FastifyRequest, response: FastifyReply) =>{
+        return new UpdateUserController().handleUpdateUser(request, response);
+    });
+
+    //ROUTE FOR DELETE USER
+    fastify.post('/user/delete', { preHandler: authenticateUser }, async (request: FastifyRequest, response: FastifyReply) => {
+        return new DeleteUserController().handleDeleteUser(request, response);
     });
 };
