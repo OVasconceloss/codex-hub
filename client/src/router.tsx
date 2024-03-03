73f9d26968c6effea9a65c7f217235f0ac2a0a49
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-03 20:20:00 +0000

    :package: feat: add the check to see if the user is logged in or not

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,13 +1,33 @@
-import React from "react";
 import Home from "./pages/home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/auth/login";
+import React, { useEffect, useState } from "react";
+import { setAccessToken } from "./services/userAuth";
+import { Navigate, Routes, Route } from "react-router-dom";
 
-export const Router: React.FC = () => {
+const ProtectedRoute: React.FC<{ path: string; element: React.ReactNode }> = ({ element }) => {
+    const [authenticated, setAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            const accessToken = await setAccessToken();
+
+            if (accessToken) setAuthenticated(true);
+        };
+        checkAuthentication();
+    }, []);
+
+    if (authenticated) return <React.Fragment>{element}</React.Fragment>;
+
+    return <Navigate to="/login" />;
+};
+
+const Router: React.FC = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<ProtectedRoute element={<Home />} path={""} />} />
+        </Routes>
     );
 };
+
+export default Router;
