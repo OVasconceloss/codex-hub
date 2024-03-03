5e717b0a1b8b9521ac21cc16d38e4ed093b40326
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-03 20:20:45 +0000

    :hammer: refactor: change the way the route file is added to the project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import './styles/index.css';
-import { Router } from './router';
+import Router from './router';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render( <Router /> );
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>
+);
