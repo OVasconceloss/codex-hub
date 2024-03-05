c1387cf7fa7b902b8e2a4c845a1a0e79bba03a39
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-05 08:52:12 +0000

    :sparkles: feat: create the function to receive and render the basic information from the posts

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/header/header";
+import { listAllPosts } from "../services/callPosts";
+import { PostLabel } from "../components/posts/postLabel";
+
+interface Post {
+    id: string,
+    title: string,
+    text: string,
+    userId: string,
+    userNickname: string,
+    created_at: Date,
+    updated_at: Date,
+    post: Post
+}
 
 const Home: React.FC = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    const handleSetPost = async () => {
+        const allposts = await listAllPosts();
+        setPosts(allposts);
+    }
+
+    useEffect(() => {
+        handleSetPost();
+    }, []);
+
     return (
         <>
         <Header />
+        <main className="p-10">
+            <section className="flex flex-col items-center">
+                {posts?.map((post, index) => {
+                    return (
+                        <PostLabel key={index} post={post} />
+                    )
+                })}
+            </section>
+        </main>
         </>
     );
 };
