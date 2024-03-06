d62b7c6555ff420ad50933c7ad4917fd0863fae6
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-06 20:11:19 +0000

    :hammer: refactor: change the entire structure of the header component

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import logoImage from "/images/codexhub-logo.png";
+import React, { useEffect, useState } from "react";
+import { DarkModeButton } from "../button/darkModeButton";
 
 export const Header: React.FC = () => {
     const [isLogged, setIsLogged] = useState(false);
@@ -12,43 +14,59 @@ export const Header: React.FC = () => {
     }, [isLogged]);
 
     return (
-        <header className="flex items-center justify-between p-5 bg-zinc-900 rounded-b-lg">
-            <div className="flex items-center space-x-10">
-                <Link to={'/'} className="flex items-center space-x-2 text-2xl font-bold text-zinc-50 
-                transition ease-linear hover:text-zinc-200">
-                    <i className="fa-solid fa-laptop-code"></i>
-                    <h1>DevNews</h1>
-                </Link>
-                <div className="space-x-5">
-                    <Link to={'/relevant'} className="text-zinc-50 text-md transition ease-linear hover:text-zinc-200">Relevant</Link>
-                    <Link to={'/about'} className="text-zinc-50 text-md transition ease-linear hover:text-zinc-200">About</Link>
-                </div>
+        <header className="flex items-center justify-between px-5 bg-zinc-900 border-b-2 border-zinc-800 rounded-b-sm
+        dark:bg-zinc-800">
+            <div className="flex items-center space-x-1">
+                <img 
+                    src={logoImage} 
+                    alt="CodeHub Logo"
+                    className="w-20 h-20 rounded-full object-cover"
+                />
+                <h1 className="text-2xl text-indigo-500 font-bold ">CodexHub</h1>
             </div>
             <div className="flex items-center space-x-5">
-                <input 
-                    type="text" 
-                    name="search" 
-                    id="search"
-                    className="w-64 p-2 text-sm outline-none rounded"
-                    placeholder="Search"
-                />
-                {!isLogged ? (
-                    <> 
-                        <Link to={'/login'}>
-                            <button className="w-20 p-1 text-zinc-50 bg-zinc-900 border border-zinc-50 rounded-md
-                            transition ease-linear hover:text-zinc-900 hover:bg-zinc-50 hover:border-zinc-900 hover:scale-110">Sign In</button>
-                        </Link>
+                <nav className="space-x-5">
+                    <Link 
+                        to={'/explorer'} 
+                        className="px-2 py-1 text-zinc-100 border-b border-zinc-900 
+                        transition ease-linear hover:border-indigo-500 hover:text-indigo-500
+                        dark:border-zinc-800 dark:hover:border-indigo-500">Explore</Link>
+                    <Link 
+                        to={'https://github.com/OVasconceloss/codex-hub'}
+                        target="_blank"
+                        className="px-2 py-1 text-zinc-100 border-b border-zinc-900 
+                        transition ease-linear hover:border-indigo-500 hover:text-indigo-500
+                        dark:border-zinc-800 dark:hover:border-indigo-500">Github</Link>
+                    <Link 
+                        to={'/aboutus'}
+                        className="px-2 py-1 text-zinc-100 border-b border-zinc-900 
+                        transition ease-linear hover:border-indigo-500 hover:text-indigo-500
+                        dark:border-zinc-800 dark:hover:border-indigo-500">About Us</Link>
+                </nav>
+                <h3 className="text-zinc-500">|</h3>
+                <div className="space-x-5">
+                    {!isLogged ? 
+                    (
+                        <>
                         <Link to={'/register'}>
-                            <button className="w-20 p-1 text-zinc-900 bg-zinc-50 border border-zinc-900 rounded-md
-                            transition ease-linear hover:text-zinc-50 hover:bg-zinc-900 hover:border-zinc-50 hover:scale-110">Sign Up</button>
+                            <button className="py-2 px-5 text-zinc-100 border rounded-sm
+                            transition ease-linear hover:bg-indigo-500 hover:border-indigo-500">Sign Up</button>
                         </Link>
-                    </> 
+                        <Link to={'/login'}>
+                            <button className="py-2 px-5 text-zinc-100 border rounded-sm
+                            transition ease-linear hover:text-indigo-500 hover:border-indigo-500">Sign In</button>
+                        </Link>
+                        </>
                     ) : (
+                        <>
                         <Link to={'/profile'}>
-                            <button className="w-20 p-1 text-zinc-900 bg-zinc-50 border border-zinc-900 rounded-md
-                            transition ease-linear hover:text-zinc-50 hover:bg-zinc-900 hover:border-zinc-50 hover:scale-110">Profile</button>
+                            <button className="py-2 px-5 text-zinc-100 border rounded-sm
+                            transition ease-linear hover:text-indigo-500 hover:border-indigo-500">Profile</button>
                         </Link>
+                        </>
                     )}
+                    <DarkModeButton />
+                </div>
             </div>
         </header>
     );
