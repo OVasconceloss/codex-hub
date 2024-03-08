8b010bd6da59a715fe21b49a0ffde0e6c02ef97d
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-08 17:18:09 +0000

    :sparkles: feat: create the user input check

--- client/src/pages/auth/register.tsx
+++ client/src/pages/auth/register.tsx
@@ -1,10 +1,77 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/footer/footer";
 import { Header } from "../../components/header/header";
 
 const Register: React.FC = () => {
-    return (
+    const [email, setEmail] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [nickName, setNickName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const validateInput = () => {
+        if (fullName == "" && nickName == "" && email == "" && password == "" && confirmPassword == "") {
+            setError(true);
+            setErrorMessage("The fields are empty");
+            return false; 
+        }
+
+        if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+            setError(true);
+            setErrorMessage("The email address is invalid. Please try again");
+            return false;
+        }
+
+        if (password === "") {
+            setError(true);
+            setErrorMessage("The password is invalid. Try again");
+            return false;
+        }
+
+        if (fullName === "" && nickName === "") {
+            setError(true);
+            setErrorMessage("The names are invalid");
+            return false;
+        } else if (fullName === "") {
+            setError(true);
+            setErrorMessage("The full name is invalid");
+            return false; 
+        } else if (nickName == "") {
+            setError(true);
+            setErrorMessage("The nick name is invalid");
+            return false;    
+        }
+
+        if (password === "") {
+            setError(true);
+            setErrorMessage("The password is invalid");
+            return false; 
+        } else if (password !== confirmPassword) {
+            setError(true);
+            setErrorMessage("The passwords don't match");
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmit = async () => {
+        if (validateInput()) {
+            console.log('CERTO');
+        }
+    };
+
+    useEffect(() => {
+        if (error) {
+            setTimeout(() => {
+                setError(false);
+            }, 2500);
+        }
+    }, [error]);
+
+    return (    
         <>
         <Header />
         <main className="flex flex-col w-screen p-5">
@@ -17,10 +84,11 @@ const Register: React.FC = () => {
                             <input 
                                 type="text"
                                 name="fullname"
-                                placeholder="Pedro Johson"
-                                className="w-[14rem] mb-10 p-2 outline-none border-2 border-zinc-300 rounded-md
-                                transition ease-linear focus:border-zinc-900 data-[fail=true]:border-red-500
-                                dark:focus:border-indigo-500"
+                                placeholder="Pedro Silva"
+                                onChange={(event) => setFullName(event.target.value)}
+                                className={`w-[14rem] mb-10 p-2 outline-none border-2 rounded-md
+                                transition ease-linear focus:border-zinc-900
+                                ${error && (fullName === "") && "border-red-500"}`}
                             />
                         </div>
                         <div className="flex flex-col">
@@ -28,10 +96,11 @@ const Register: React.FC = () => {
                             <input 
                                 type="text"
                                 name="nickname"
-                                placeholder="opedrorealjohson"
-                                className="w-[15rem] mb-10 p-2 outline-none border-2 border-zinc-300 rounded-md
-                                transition ease-linear focus:border-zinc-900 data-[fail=true]:border-red-500
-                                dark:focus:border-indigo-500"
+                                placeholder="opedrosilva"
+                                onChange={(event) => setNickName(event.target.value)}
+                                className={`w-[14rem] mb-10 p-2 outline-none border-2 rounded-md
+                                transition ease-linear focus:border-zinc-900
+                                ${error && (nickName === "") && "border-red-500"}`}
                             />
                         </div>
                     </div>
@@ -41,9 +110,10 @@ const Register: React.FC = () => {
                             type="email"
                             name="email"
                             placeholder="example@gmail.com"
-                            className="w-[30rem] mb-10 p-2 outline-none border-2 border-zinc-300 rounded-md
-                            transition ease-linear focus:border-zinc-900 data-[fail=true]:border-red-500
-                            dark:focus:border-indigo-500"
+                            onChange={(event) => setEmail(event.target.value)}
+                            className={`w-[30rem] mb-10 p-2 outline-none border-2 rounded-md
+                            transition ease-linear focus:border-zinc-900
+                            ${error && !email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/) && "border-red-500"}`}
                         />
                     </div>
                     <div className="flex flex-col">
@@ -52,10 +122,11 @@ const Register: React.FC = () => {
                             <label htmlFor="password" className="mb-1 dark:text-zinc-100">Password</label>
                             <input 
                                 type="password"
-                                name="password" 
-                                className="w-[14rem] mb-10 p-2 outline-none border-2 border-zinc-300 rounded-md
-                                transition ease-linear focus:border-zinc-900 data-[fail=true]:border-red-500
-                                dark:focus:border-indigo-500"
+                                name="password"
+                                onChange={(event) => setPassword(event.target.value)}
+                                className={`w-[14rem] mb-10 p-2 outline-none border-2 rounded-md
+                                transition ease-linear focus:border-zinc-900
+                                ${error && (password === "" || password !== confirmPassword) && "border-red-500"}`}
                             />
                         </div>
                         <div className="flex flex-col">
@@ -63,14 +134,16 @@ const Register: React.FC = () => {
                             <input 
                                 type="password"
                                 name="confirm"
-                                className="w-[15rem] mb-10 p-2 outline-none border-2 border-zinc-300 rounded-md
-                                transition ease-linear focus:border-zinc-900 data-[fail=true]:border-red-500
-                                dark:focus:border-indigo-500"
+                                onChange={(event) => setConfirmPassword(event.target.value)}
+                                className={`w-[15rem] mb-10 p-2 outline-none border-2 rounded-md
+                                transition ease-linear focus:border-zinc-900
+                                ${error && (confirmPassword === "" || password !== confirmPassword) && "border-red-500"}`}
                             />
                         </div>
                     </div>
                     </div>
                     <button
+                        onClick={handleSubmit}
                         className="w-[30rem] p-2 my-5 text-zinc-50 border border-zinc-900 rounded-md bg-zinc-900
                         transition ease-linear hover:border-zinc-900 hover:bg-transparent hover:text-zinc-900
                         dark:border-indigo-500 dark:text-indigo-500 dark:hover:bg-indigo-500 dark:hover:text-zinc-100">
@@ -88,6 +161,11 @@ const Register: React.FC = () => {
         <div className="w-full absolute bottom-0 flex items-center justify-center bg-zinc-900">
             <Footer /> 
         </div>
+        { (error) && (
+            <div className="w-[25rem] fixed bottom-5 right-5 bg-red-500 text-zinc-50 py-2 px-4 rounded shadow text-center">
+                {errorMessage}
+            </div>
+        )}
         </>
     );
 };
