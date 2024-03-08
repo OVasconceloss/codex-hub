6088e9cfa1ad6d6d3f46c342f82e95492f0f6bf7
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-08 08:26:43 +0000

    :lipstick: style: change the dark mode button color (hover)

--- client/src/components/button/darkModeButton.tsx
+++ client/src/components/button/darkModeButton.tsx
@@ -19,7 +19,7 @@ export const DarkModeButton = () => {
     return (
         <button 
             className="w-20 rounded-sm py-[6px] px-5 border bg-transparent text-zinc-100 text-lg transition ease-linear 
-            hover:bg-blue-500 hover:text-zinc-100 hover:border-indigo-500 dark:text-zinc-100 dark:hover:bg-indigo-500 
+            hover:bg-indigo-500 hover:text-zinc-100 hover:border-indigo-500 dark:text-zinc-100 dark:hover:bg-indigo-500 
             dark:hover:text-zinc-100 dark:hover:border-indigo-500" 
             onClick={toggleDarkMode}>
             <i className={`fa-solid fa-${darkMode ? 'sun' : 'moon'}`}></i>
