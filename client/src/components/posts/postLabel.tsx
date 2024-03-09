c96660a6bde0c2cda42e9f133d499c092028d503
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-03-09 11:35:35 +0000

    :package: feat: use the date-fns library to format the post's update date

--- client/src/components/posts/postLabel.tsx
+++ client/src/components/posts/postLabel.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { formatDistanceToNow } from 'date-fns';
 
 interface Post {
     id: string,
@@ -16,15 +17,18 @@ interface LabelPost {
 }
 
 export const PostLabel: React.FC<LabelPost> = ({ post }) => {
+    const createdTime = formatDistanceToNow(new Date(post.updated_at), { addSuffix: true });
+
     return (
-        <div className="w-[50rem] p-5 my-2 bg-zinc-100 rounded-md scale-100 transition ease-linear hover:scale-105">
+        <div className="w-[60rem] p-3 my-2 bg-zinc-100 rounded-md scale-100 
+        transition ease-linear hover:scale-105 dark:bg-zinc-800">
             <Link to={`posts/${post.userId}`}>
-                <h1 className="hover:underline">{ post.title }</h1>
+                <h1 className="text-xl dark:text-zinc-100 hover:underline">{ post.title }</h1>
             </Link>
-            <div className="w-48 flex items-center justify-between">
-                <h3 className="text-sm">{ post.userNickname }</h3>
-                <h3 className="text-sm">-</h3>
-                <h3 className="text-sm">15 horas atrás</h3>
+            <div className="w-72 mt-2 flex items-center space-x-2">
+                <h3 className="text-md dark:text-zinc-100">{ post.userNickname }</h3>
+                <h3 className="text-md dark:text-zinc-100">|</h3>
+                <h3 className="text-md dark:text-zinc-100">{ createdTime }</h3>
             </div>
         </div>
     );
